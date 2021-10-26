8e75ccbe1664f5386b1074623996886068d715e8
Lovecase Singh <76085767+Lovecase@users.noreply.github.com>
2021-10-26 12:25:00 +0530

    Create Sum_of_diagonals_Array.c

---
+++ C-Random/Sum_of_diagonals_Array.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main()
+{    
+	int i;
+	int j;
+	int n;
+    int a[100][100];
+    int d1=0;
+    int d2=0;
+	printf("Enter size of square matrix \n");
+	scanf("%d",&n);
+	printf("\n Enter elements in array \n");
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n array is \n");
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf(" %d ",a[i][j]);
+		}
+	printf("\n")	;
+	}
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			{
+				d1=d1+a[i][j];
+			}
+
+		}
+	}
+    	for(i=0;i<n;i++)
+	    {
+		    for(j=0;j<n;j++)
+		    {
+			    if( i + j == n-1)
+			    {
+			    	d2=d2+a[i][j];
+			    }
+            }
+    }	
+	printf("\n sum of diagonal 1 is %d and diagonal 2 is %d \n",d1,d2);
+}
